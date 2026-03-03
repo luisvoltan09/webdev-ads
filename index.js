fec2430e3f0af5e95cd13622bdbbffaa687e8f48
unilago <unilago@edu.br>
2026-03-03 20:26:40 -0300

    luislab

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
-const express = require("express")
+const express = require('express')
+const { engine } = require('express-handlebars')
+const bodyparser = require('body-parser')
 
 const app = express()
 
-app.use("/", function (req, res){
-    res.send("<h1>Funcionou</h1>")
+app.use(bodyParser.json)
+
+app.engine('handlebars', engine({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+app.get("/", (req, res) => {
+  res.render('index')
+})
+
+app.get("/sobre/:id", (req, res) => {
+  res.render("sobre", { id })
 })
 
 app.listen(3000, () => {
-    console.log('servidor rodando em http://localhost:3000')
+  console.log('Servidor rodando em http://localhost:3000')
 })
